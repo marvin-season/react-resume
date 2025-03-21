7fd22c5eb6f865516bd7fa1a1bb28cc7a82ae38c
marvin <sea-son@foxmail.com>
2025-03-21 14:24:57 +0800

    feat: resume container

--- src/components/resume/index.tsx
+++ src/components/resume/index.tsx
@@ -5,7 +5,7 @@ import Experiences from "@/components/resume/experiences.tsx";
 
 const Resume = () => {
   return (
-    <div className="max-w-4xl mx-auto p-6">
+    <div className="m-4 max-w-4xl mx-auto py-6 px-16 rounded-lg shadow-[0_4px_20px_rgba(0,0,0,0.25)]">
       <Header/>
       <Advantage/>
       <Experiences/>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,19 @@ import type { Config } from 'tailwindcss'
 export default {
   content: ['./src/**/*.tsx', './src/**/*.jsx', 'node_modules/@marvin/react-ai/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#2A5CAA',
+        },
+        secondary: {
+          DEFAULT: '#6C9BD2',
+        },
+        accent: {
+          DEFAULT: '#FFC847',
+        }
+      }
+    },
   },
   plugins: [],
 } satisfies Config
