d2afa54b3172a686e55441396d13ddbcfd7dc94b
marvin <sea-son@foxmail.com>
2025-03-24 10:36:06 +0800

    feat: 工作经验样式优化

--- src/components/resume/experiences.tsx
+++ src/components/resume/experiences.tsx
@@ -18,47 +18,48 @@ export default function Experiences() {
             </div>
             <span className="text-gray-500">{experience.date}</span>
           </div>
-
-          {experience.projects.map((project, idx) => (
-            <div key={idx} className="mb-6 draw-body">
-              <div className={"flex gap-2 items-center"}>
-                <h3 className="text-xl font-semibold">{project.name}</h3>
-                <p className="text-gray-500">{project.description}</p>
-              </div>
-              <div className="mt-2">
-                {project.responsibilities.map((responsibility, i) => (
-                  <div key={i} className="pt-2 pl-4 text-gray-700">
-                    <strong>{responsibility.area}:</strong>
-                    {responsibility.stack.map((stack, j) => (
-                      <div key={j} className="text-link">
-                        {stack}
-                      </div>
-                    ))}
-                    <div className="mt-2 space-y-1">
-                      {responsibility.tasks.map((task, k) => (
-                        <div key={k} className="text-gray-600">
-                          {typeof task === "string" ? (
-                            task
-                          ) : (
-                            <div className={"mb-4 pt-2"}>
-                              <div className={"font-bold"}>{task.feature}</div>
-                              <div className={"mb-3 pt-1"}>
-                                {task.details.map((detail, i) => (
-                                  <div key={i} className={"text-sm"}>
-                                    {detail}
-                                  </div>
-                                ))}
-                              </div>
-                            </div>
-                          )}
+          <div className={"draw-body"}>
+            {experience.projects.map((project, idx) => (
+              <div key={idx} className="mb-6">
+                <div className={"flex gap-2 items-center"}>
+                  <h3 className="text-xl font-semibold">{project.name}</h3>
+                  <p className="text-gray-500">{project.description}</p>
+                </div>
+                <div className="mt-2">
+                  {project.responsibilities.map((responsibility, i) => (
+                    <div key={i} className="pt-2 pl-4 text-gray-700">
+                      <strong>{responsibility.area}:</strong>
+                      {responsibility.stack.map((stack, j) => (
+                        <div key={j} className="text-link">
+                          {stack}
                         </div>
                       ))}
+                      <div className="mt-2 space-y-1">
+                        {responsibility.tasks.map((task, k) => (
+                          <div key={k} className="text-gray-600">
+                            {typeof task === "string" ? (
+                              task
+                            ) : (
+                              <div className={"mb-4 pt-2"}>
+                                <div className={"font-bold"}>{task.feature}</div>
+                                <div className={"mb-3 pt-1"}>
+                                  {task.details.map((detail, i) => (
+                                    <div key={i} className={"text-sm"}>
+                                      {detail}
+                                    </div>
+                                  ))}
+                                </div>
+                              </div>
+                            )}
+                          </div>
+                        ))}
+                      </div>
                     </div>
-                  </div>
-                ))}
+                  ))}
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       ))}
     </>
