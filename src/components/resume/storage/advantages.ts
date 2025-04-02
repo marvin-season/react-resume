27cd33b5cc0839bb2845282a243c9cde946214b5
marvin <sea-son@foxmail.com>
2025-04-02 10:42:14 +0800

    feat: 简历优化

--- src/components/resume/storage/advantages.ts
+++ src/components/resume/storage/advantages.ts
@@ -5,47 +5,37 @@ export default [
       {
         title: "技术栈",
         details: [
-          "精通JavaScript/ES6开发, 能够熟练使用HTML/CSS/JS开发项目",
-          "精通React开发，理解其原理，能够使用React及其技术栈开发高性能网站",
+          "精通JavaScript/ES6, 能够熟练使用HTML/CSS/JS开发项目",
+          "精通React + TypeScript + Antd开发，理解其原理，能够使用React及其技术栈开发高性能网站",
+          "熟练使用 ElementUI/Antd + Vue3 + TypeScript开发，开发维护管理后台",
+          "状态管理 ReactContext/ReduxToolkit/Zustand/Pinia/Vuex等等使用经验",
+          "多种技术栈使用经验：TankQuery + Formik + Lodash + ReactFlow + Fetch + Websocket + SSE ...",
           "熟练使用Vite，熟练项目模块化与工程化，架构并搭建多个大型项目（Mono-Repo）、组件库、工具，实现代码复用，提高开发和交付的效率",
-          "后端开发经验，如Java(SpringBoot)、C/C++、Python、NodeJs(ExpressJS)，其中有一年的Java开发经验",
+          "项目国际化经验(i18n) + 新用户引导 + 网站SEO优化",
           "前端性能优化经验，如：预加载、懒加载、CRP关键路径优化、虚拟化等等",
           "NestJs, NextJs, Strapi, Docusaurus等多种新兴技术栈的使用",
-          "熟练使用Vue2/3，理解其原理",
-          "跨平台开发实践：Electron 与 UniApp",
-          "AI提效，使用AI辅助开发超1年"
+          "跨平台开发实践：UniApp",
+          "AI相关工具, RAG, Ollama, 深度思考等前端特性开发",
+          "后端开发经验，如Java(SpringBoot)、C/C++、Python、NodeJs(ExpressJS)，其中有一年的Java开发经验",
         ],
       },
       {
-        title: "专业性与系统性",
-        details: [
-          "计算机科学背景：本科信息安全专业，转向软件开发方向",
-          "重点本科院校：武汉科技大学（湖北高校排名前八）",
-          "计算机体系化学习：操作系统、网络安全、数据结构与算法、数据库及高级编程语言（C/C++/Java）",
-          [
-            "4年Web开发实际经验",
-            "1年Java 后端开发经验",
-            "1年小程序开发实践经验",
-          ].join(',')
-        ],
-      },
-      // {
-      //   title: "开发经验",
-      //   details: [
-      //     "4年 Web开发实际经验",
-      //     "1年 Java 后端开发经验",
-      //     "1年 小程序开发实践经验",
-      //   ],
-      // },
-      {
-        title: "技术探索",
+        title: "技术延展与参考",
         details: [
           {
             description: "前端工程化与模块化实践：Vite(Rollup) + Babel + Mono-Repo",
             links: [
               { label: "👌 AIO-Modal", href: "https://www.npmjs.com/package/aio-modal" },
+            ],
+          },
+          {
+            description: "国际化方案",
+            links: [
               { label: "🚀 Babel", href: "https://github.com/marvin-season/i18n-ast" },
             ],
+          },
+          {
+            description: "新用户引导开发",
           },
           {
             description: "文档网站搭建与知识管理",
@@ -62,7 +52,7 @@ export default [
             ],
           },
           {
-            description: "精通 React 技术栈：React Hooks、Next.js 及其生态",
+            description: "精通React技术栈：React Hooks、Next.js 及其生态",
             links: [
               {
                 label: "🪜 React原理",
@@ -93,15 +83,6 @@ export default [
             description: "错误追踪与监控系统开发",
             links: [
               { label: "Sentry", href: "https://github.com/marvin-season/sentry-react" },
-            ],
-          },
-          {
-            description: "Web 性能优化与用户体验改进",
-            links: [
-              {
-                label: "UPP (User Perceived Performance)",
-                href: "https://developer.mozilla.org/en-US/docs/Web/Performance/Guides/Fundamentals",
-              },
             ],
           },
         ],
--- src/components/resume/storage/baseinfo.tsx
+++ src/components/resume/storage/baseinfo.tsx
@@ -3,7 +3,8 @@ export default {
   items: [
     { label: "姓名", value: "马文澍(Shù)" },
     { label: "职位", value: "Web开发工程师" },
-    { label: "学历", value: "一本/武汉科技大学" },
+    { label: "工作经验", value: "4年" },
+    { label: "学历", value: "重点一本/武汉科技大学" },
     { label: "邮箱", value: "2764876579@qq.com"},
     { label: "电话", value: "15623192717" },
     { label: "个人网站", value: "http://fuelstack.icu" },
